4084b335002e84b6cc18e1024e5cbdedc7ade418
nitya <nityabhaskar21@gmail.com>
2021-07-01 17:27:23 +0530

    Implemented update posts but some unhandled errors in HttpError res

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { ViewallPostsComponent } from './viewall-posts/viewall-posts.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ViewPostByIdComponent } from './view-post-by-id/view-post-by-id.component';
 import { CreatePostComponent } from './create-post/create-post.component';
+import { UpdatePostComponent } from './update-post/update-post.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/posts', pathMatch: 'full' },
   { path: 'posts/create', component: CreatePostComponent },
+  { path: 'posts/update/:id', component: UpdatePostComponent },
   { path: 'posts/:id', component: ViewPostByIdComponent },
   { path: 'posts', component: ViewallPostsComponent },
   { path: '**', component: PageNotFoundComponent }
