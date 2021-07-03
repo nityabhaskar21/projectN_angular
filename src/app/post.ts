301552fa697670bdd7617ec38e9b1b57ff321253
nitya <nityabhaskar21@gmail.com>
2021-07-03 10:14:08 +0530

    added description

--- src/app/post.ts
+++ src/app/post.ts
@@ -2,6 +2,8 @@ export class Post {
   id: string;
   title: string;
   tags: Array<string>;
+  description: string;
+  category: string;
   author: string;
   createdAt: string;
   updatedAt: string;
